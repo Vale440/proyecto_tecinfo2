fc65968f1c782079d37cbd235301cd0a88246049
Vale440 <a280292440@my.uvm.edu.mx>
2025-05-08 07:35:10 -0600

    carga de archivos

---
+++ mi_script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const miBoton = document.getElementById('miBoton');
+
+    miBoton.addEventListener('click', function() {
+        alert('¡Bienvenido a Vida Marina!');
+        // Aquí puedes agregar más funcionalidades para el botón
+    });
+});
